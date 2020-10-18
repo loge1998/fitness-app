f1f230bd062db3aed31c9afd3b033c2336f5ffd1
loge1998 <logeshbalu1998@gmail.com>
2020-10-18 18:32:20 +0530

    Token used to get user details in task service

--- app/routes/tasks.py
+++ app/routes/tasks.py
@@ -1,6 +1,6 @@
 from app.dto.task import Task
-from fastapi import APIRouter
-
+from fastapi import APIRouter, Depends
+from app.core.auth import get_current_user
 from app.dto.task_response import TaskResponse
 from app.services.tasks import add_task, get_task_by_id, get_all_tasks_for_user
 from typing import List
@@ -9,15 +9,15 @@ router = APIRouter()
 
 
 @router.post("/tasks", tags=["task"], response_model=TaskResponse)
-def create_user(payload: Task) -> TaskResponse:
-    username = ''  # This has to be passed from token
+def add_task_to_db(payload: Task, current_user=Depends(get_current_user)) -> TaskResponse:
+    username = current_user['username']  # This has to be passed from token
     task = add_task(**vars(payload), username=username)
     return TaskResponse(**task)
 
 
 @router.get("/tasks", tags=["task"], response_model=List[TaskResponse])
-def create_user() -> List[TaskResponse]:
-    username = '' # This has to be passed from token
+def create_user(current_user=Depends(get_current_user)) -> List[TaskResponse]:
+    username = current_user['username']  # This has to be passed from token
     tasks = get_all_tasks_for_user(username)
     return [TaskResponse(**task) for task in tasks]
 
